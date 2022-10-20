e53291fc3583863e3e6fc0d01a48de64953942ab
wogus4048 <wogus4048@naver.com>
2022-10-20 12:06:23 +0900

    ㅅ수정

--- src/Week3/marathon.java
+++ src/Week3/marathon.java
@@ -2,22 +2,27 @@ package Week3;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 public class marathon {
 
     public static String solution(String[] participant, String[] completion) {
         String answer = "";
-        Collections.sort(Arrays.asList(participant));  // Collections.sort는 최악 nlogn     // Arrays.sort는 최악 n^2  //둘다 통과는됨
-        Collections.sort(Arrays.asList(completion));
+        List<String> par = Arrays.asList(participant);
+        List<String> com = Arrays.asList(completion);
 
-        for(int i=0;i<participant.length-1;i++)
+        Collections.sort(par);  // Collections.sort는 최악 nlogn     // Arrays.sort는 최악 n^2  //둘다 통과는됨
+        Collections.sort(com);
+
+        for(int i=0;i< par.size()-1;i++)
         {
-            if(!participant[i].equals(completion[i]))
+            if(!par.get(i).equals(com.get(i)))
             {
-                return participant[i];
+                return par.get(i);
             }
         }
-        answer = participant[participant.length-1];
+        answer = par.get(par.size()-1);
+
 
 
         return answer;
